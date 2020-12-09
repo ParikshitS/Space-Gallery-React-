77e22bcc93d947f71c57fa309829294da9ca996d
Parikshit <pshembekar100@gmail.com>
2020-12-09 00:28:28 -0500

    Adding updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
         <>
             <nav className="navbar">
@@ -9,7 +13,32 @@ function Navbar() {
                     <Link to="/" className='navbar-logo'>
                         Space <i class="fas fa-space-shuttle"></i>
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/panets' className='nav-links' onClick={closeMobileMenu}>
+                            Planets
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/galaxies' className='nav-links' onClick={closeMobileMenu}>
+                            Galaxies
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         </>
     );
